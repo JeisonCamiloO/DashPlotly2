186c81e3c47f4f9a336581b9ed8e6f3c81b0799f
jhonUniandes <jhongerardovega@gmail.com>
2023-10-30 19:19:38 -0500

    Transformacion csv a sql y funcionando en la maquina virtual y RDS

---
+++ bd-conexion.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+engine = psycopg2.connect(
+dbname="exito",
+user="postgres",
+password="proyecto",
+host="db-proyecto2.cyfv7a2dzl8k.us-east-1.rds.amazonaws.com",
+port='5432'
+)
+
+print(engine)
+
+cursor = engine.cursor()
+
+consulta = "SELECT Target FROM ESTUDIANTE WHERE Admission_grade >= 170;"
+
+cursor.execute(consulta)
+result = cursor.fetchall()
+print(result)
