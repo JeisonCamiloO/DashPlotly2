ee55f5622d0a73a5aa0bdcbc44b3c67c0eea5b2a
JeisonCamiloO <142942540+JeisonCamiloO@users.noreply.github.com>
2023-11-07 16:39:22 -0500

    Update bd_conexion.py

--- despliegue/bd_conexion.py
+++ despliegue/bd_conexion.py
@@ -2,8 +2,8 @@ import psycopg2
 
 engine = psycopg2.connect(
 dbname="exito",
-user="xxxxxx",
-password="proyecto",
+user="postgres",
+password="xxxxx",
 host="db-proyecto2.cyfv7a2dzl8k.us-east-1.rds.amazonaws.com",
 port='5432'
 )
