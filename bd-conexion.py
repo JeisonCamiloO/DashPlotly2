acd48b3eb1d7250914994d99083ac9cc2782f4b9
jhonUniandes <jhongerardovega@gmail.com>
2023-11-07 16:34:48 -0500

    hackeado

--- bd-conexion.py
+++ bd-conexion.py
@@ -2,7 +2,7 @@ import psycopg2
 from dotenv import load_dotenv # pip install python-dotenv
 import os
 
-env_path="C:/Users/jgvm/OneDrive/Escritorio/Maestria/Primer Semestre (2023-2)/Analitica Computacional para la Toma de Decisiones/Proyecto/DashPlotly2/env/app.env"
+env_path="env/app.env"
 
 # load env 
 load_dotenv(dotenv_path=env_path)
@@ -13,13 +13,6 @@ PASSWORD=os.getenv('password')
 HOST=os.getenv('host')
 PORT=os.getenv('port')
 DBNAME=os.getenv('dbname')
-
-print(USER)
-print(PASSWORD)
-print(HOST)
-print(PORT)
-print(DBNAME)
-
 
 engine = psycopg2.connect(
 dbname = DBNAME,
